fd28a69ce6e024f434560efeb545e3d92a86b79b
Charlene Jan <charlenejan@MacBook-Air-de-Charlene.local>
2025-04-11 10:52:51 +0200

    Ex3

---
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodoItemTest.java
@@ -0,0 +1,29 @@
+package com.acme.todolist.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TodoItemTest {
+
+    @Test
+    void shouldNotMarkItemAsLateIfLessThan24HoursOld() {
+        TodoItem item = new TodoItem("1", Instant.now().minus(23, ChronoUnit.HOURS), "Content");
+        assertEquals("Content", item.finalContent());
+    }
+
+    @Test
+    void shouldMarkItemAsLateIfExactly24HoursOld() {
+        TodoItem item = new TodoItem("2", Instant.now().minus(24, ChronoUnit.HOURS), "Content");
+        assertEquals("[LATE!] Content", item.finalContent());
+    }
+
+    @Test
+    void shouldMarkItemAsLateIfMoreThan24HoursOld() {
+        TodoItem item = new TodoItem("3", Instant.now().minus(25, ChronoUnit.HOURS), "Content");
+        assertEquals("[LATE!] Content", item.finalContent());
+    }
+}
